bba66446461bb0b55bc90b98945a499e231f4e68
nirupan <nirupanjoej@gmail.com>
2021-07-14 13:21:07 +0530

    introduced a flexBox

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { React, useEffect } from 'react';
 import './App.scss';
 import SampleService from './services/sample';
 import FlexSelect from './components/flexSelect';
+import FlexBox from './components/flexBox';
 
 const App = () => {
 	useEffect(SampleService.sayHai, []);
 
 	return (
 		<div className="App">
-			<div>Count: { FlexSelect() }</div>
+			{ FlexSelect() }
+			{ FlexBox() }
 		</div>
 	);
 };
---
+++ src/components/flexBox.js
@@ -0,0 +1,9 @@
+import { React } from 'react';
+
+const FlexBox = () =>
+	<div className="container">
+		<div className="box1"/>
+		<div className="box2"/>
+	</div>;
+
+export default FlexBox;
